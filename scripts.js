691cb46c4cbb925d7e14a4f28edb1a2ff52de9e6
snp235 <108408380+snp235@users.noreply.github.com>
2023-10-25 15:53:30 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function navigate() {
+    var selection = document.querySelector('select').value;
+    switch (selection) {
+        case '1':
+            window.location.href = 'https://my.matterport.com/show/?m=UDokCSSxELg';
+            break;
+        case '2':
+            window,location.href = '';//link do site
+            break; 
+        case '3':
+            window.location.href = '';//link do site
+            break;
+        // Adicione mais casos conforme necessário
+    }
+}
